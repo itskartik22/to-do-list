b0a8136155b500b249dee2909231047889200ad1
thakur_kartik <thakurkartik2262@gmail.com>
2023-02-21 22:21:35 +0530

    host

---
+++ script.js
@@ -0,0 +1,66 @@
+//button
+taskAddBtn = document.querySelector('.add__btn')
+taskDelBtn = document.querySelectorAll('.del__task')
+taskCheckBox = document.querySelector('task__checkbox')
+
+//input
+taskInput = document.querySelector('.task__input')
+taskBoxContainer = document.querySelector('.task__container')
+//type of Array
+taskArray = []
+
+function addTaskItem() {
+    const taskText = taskInput.value;
+    taskInput.value = ''
+    if (taskText.length == 0) return;
+    taskArray.push(`
+    <div class="task__item" id="task${taskArray.length}">
+    <div class="task__checkbox" > </div>
+    <div class="task">
+        <p class="task__text">${taskText}</p>
+    </div>
+    <div class="del__task">
+        <div class="upper__bar"></div>
+        <div class="lower__bar"></div>
+    </div>
+</div>`);
+    addTaskDisplay();
+    addEvent()
+    updateDeletion()
+
+}
+taskAddBtn.addEventListener('click', addTaskItem)
+
+function addTaskDisplay() {
+    taskBoxContainer.innerHTML += taskArray[taskArray.length - 1]
+}
+
+//Deleteion
+function updateDeletion(){
+    taskDelBtn = document.querySelectorAll('.del__task')
+    taskDelBtn.forEach(element => {
+        element.addEventListener('click', () => {
+            // element.parentElement.style.display = 'none'
+            element.parentElement.classList.add('fade_animation')
+            setTimeout(() => {
+                element.parentElement.style.display = 'none'
+            },2000)
+        })
+    });
+}
+
+//complete Taks
+function addEvent() {
+    taskItem = document.querySelectorAll('.task__item')
+    taskItem.forEach(element => {
+        element.addEventListener('click', () => {
+            element.style.textDecoration = "line-through"
+            element.childNodes[1].style.backgroundColor = 'green'
+        })
+    })
+}
+
+//Clear Button
+document.getElementsByClassName('clear__btn')[0].onclick = () => {
+    taskBoxContainer.innerHTML = ''
+}
